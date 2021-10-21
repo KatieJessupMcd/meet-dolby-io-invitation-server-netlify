fb0945045f35c1c75412462af3929470bb9b1177
Katie Gray <katie.gray@dolby.com>
2021-10-21 09:20:39 -0700

    updating to use as invitation server for meet dolby io

---
+++ functions/intivation-server.js
@@ -0,0 +1,27 @@
+const { EVENT_CODE } = process.env;
+
+const querystring = require('querystring');
+
+exports.handler = async (event, context) => {
+  // Only allow POST
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  // When the method is POST, the name will no longer be in the event’s
+  // queryStringParameters – it’ll be in the event body encoded as a query string
+  const params = querystring.parse(event.body);
+  const eventCode = params.eventCode;
+
+  if (eventCode === EVENT_CODE) {
+    return {
+      statusCode: 200,
+      body: 'Event Code Correct',
+    };
+  } else {
+    return {
+      statusCode: 401,
+      body: 'Event Code Incorrect',
+    };
+  }
+};
